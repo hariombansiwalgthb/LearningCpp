6505ce7808e162ab8da8ec778c8fb2785be086d0
HB <144981798+hariombansiwalgthb@users.noreply.github.com>
2024-06-18 10:55:06 +0530

    Update PrintSumOfElementsOfVector.cpp

--- PrintSumOfElementsOfVector.cpp
+++ PrintSumOfElementsOfVector.cpp
@@ -1,1 +1,30 @@
 
+#include <iostream>
+#include <vector>
+template <typename Container>
+void printContainer(const Container& container)
+{
+	std::cout << "Elements of container :  ";
+	for (const auto & element : container)
+	{
+		std::cout << element << " ";
+	}
+}
+
+const int& SumOfAllElements(const std::vector<int>& vec) {
+	printContainer(vec);
+	int sum{};
+	for (auto& elem: vec)
+	{
+		sum += elem;
+	}
+	return sum;
+}
+
+int main() {
+	
+	std::vector<int> vec{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	auto& iSum = SumOfAllElements(vec);
+	std::cout<<"\nSum = " << iSum<<std::endl;
+    return 0;
+}
